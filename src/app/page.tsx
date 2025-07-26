e7428559f6f1933c6691fbe1b039c42c2579c619
Richard Kim <rukim@hawaii.edu>
2025-07-25 20:15:13 -1000

    update color scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { PeopleFill } from 'react-bootstrap-icons';
 
-/** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+    <Container fluid className="py-5 bg-light text-center">
+      <Row className="justify-content-center align-items-center">
+        <Col md={6}>
+          <PeopleFill size={120} className="mb-4 text-success" />
+          <h1 className="mb-3">Find a club for you</h1>
+          <Button variant="success" size="lg">Explore Clubs</Button>
         </Col>
       </Row>
     </Container>
