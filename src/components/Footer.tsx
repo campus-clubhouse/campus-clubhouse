d8e8bac6fc95bec7b145322cc44e9dce45acb221
Dominic <97217529+deuzd@users.noreply.github.com>
2025-08-15 05:06:42 -1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,6 @@ const Footer = () => (
         <br />
         Honolulu, HI 96822
         <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
       </Col>
     </Container>
   </footer>
